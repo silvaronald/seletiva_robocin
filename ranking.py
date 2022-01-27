2a30531b83ee645146da8bc483b85eda90311773
José Ronald <jronaldsilva11@gmail.com>
2022-01-26 21:03:29 -0300

    ranking system updated

--- ranking.py
+++ ranking.py
@@ -22,7 +22,7 @@ def main():
         for i in range(10):
             # The formula used to calculate the score is: (stamina + tackles) * effort
             # The final score is the mean of all matches scores
-            scores[i] += ((results[i][0] + results[i][2]) * results[i][1]) / logs_len
+            scores[i] += results[i][0] * results[i][2] * results[i][1] / logs_len
 
     # Plots graph with players' scores
     plt.bar(players, scores)
